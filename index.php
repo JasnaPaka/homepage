90c38523360874c569b15d951d074786d4eec7d2
Pavel Cvrček <jasnapaka@jasnapaka.com>
2015-11-29 12:17:10 +0100

    Úvodní podoba webu.

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Pavel Cvrček [JasnaPaka]</title>
+
+  <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+  
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <div class="container-fluid">
+      
+      <div class="row">
+          <h1>Pavel Cvrček</h1>
+          
+          <h2>O autorovi</h2>
+          
+          <p>Jsem vývojář a ve svém volném čase pomáhám realizovat několik projektů. Aktivně přispívám do
+              <a href="http://www.openstreetmap.org/">OpenStreetMap</a> a 
+              projektu <a href="http://www.mozilla.org/">Mozilla</a>. Pokud mě potřebujete kontaktovat,
+              pište na <a href="mailto:jasnapaka@jasnapaka.com">jasnapaka@jasnapaka.com</a>.</p>
+          
+          <h2>Projekty</h2>
+          
+          <dl>
+              <dt><a href="http://krizkyavetrelci.plzne.cz/">Křížky a vetřelci</a></dt>
+              <dd>Mapuje drobné umění na území města Plzně. </dd>
+
+              <dt><a href="http://cyklomapa.plzne.cz/">Cyklomapa Plzně</a></dt>
+              <dd>Komunitní cyklomapa Plzně a okolí.</dd>
+             
+              <dt><a href="http://osm.jasnapaka.com/">Offline mapy pro Android</a></dt>
+              <dd>Mapy České republiky a Slovenska založené na OpenStreetMap.</dd>
+
+              <dt><a href="http://mozbackup.jasnapaka.com/">MozBackup</a></dt>
+              <dd>Nástroj na zálohování dat aplikací Mozilla.</dd>              
+          </dl>
+          
+          <h2>Profily na webu</h2>
+          
+          <p id="logos">
+              <a href="https://cz.linkedin.com/in/pavelcvrcek" class="logo"
+                    title="Zobrazit pracovní profil na LinkedIn">
+                  <img src="images/linkedin.png" alt="Logo LinkedIn" />
+              </a>
+
+              <a href="https://www.facebook.com/jasnapaka" class="logo"
+                    title="Zobrazit soukromý profil na Facebooku">
+                  <img src="images/facebook.png" alt="Logo Facebooku" />
+              </a>      
+              
+              <a href="https://github.com/JasnaPaka" class="logo"
+                    title="Zobrazit profil na GitHubu">
+                  <img src="images/github.png" alt="Logo GitHubu" />
+              </a>              
+          </p>
+      </div>
+  </div>
+
+</body>
+</html>
